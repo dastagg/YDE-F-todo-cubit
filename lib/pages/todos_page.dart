86b8d858a7700a47028426b1e2dbcb36723df3a1
dastagg:opus5 <dastagg@stagglinux.com>
2022-04-09 19:53:19 -0400

    Show first two widgets on page.
    In order to make this work, there are quite a few other additions as
    well.
    
    Updated the structure of the app by adding a widgets directory.
    Created the TodoHeader and CreateTodo widgets
    Added those to the TodosPage
    
    The number of Todos wasn't displaying correctly; modified
    active_todo_count_cubit and todo_list_cubit so the 3 hardcoded todos
    show up in the counter.

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/create_todo.dart';
+import '../widgets/todo_header.dart';
+
 class TodosPage extends StatelessWidget {
   const TodosPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('TODO'),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+            child: Column(
+              children: [
+                TodoHeader(),
+                CreateTodo(),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
