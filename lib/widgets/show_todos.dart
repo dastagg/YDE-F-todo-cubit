25526fc40e3d948edcc95e111fe7c69d6bb7a859
dastagg:opus5 <dastagg@stagglinux.com>
2022-04-09 21:34:46 -0400

    Add Dismissible and AlertBox.

--- lib/widgets/show_todos.dart
+++ lib/widgets/show_todos.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_cubit/cubits/filtered_todos/filtered_todos_cubit.dart';
+import 'package:todo_cubit/cubits/todo_list/todo_list_cubit.dart';
+
+import '../models/todo_model.dart';
 
 class ShowTodos extends StatelessWidget {
   const ShowTodos({Key? key}) : super(key: key);
@@ -14,14 +17,77 @@ class ShowTodos extends StatelessWidget {
       shrinkWrap: true,
       itemCount: todos.length,
       separatorBuilder: (BuildContext context, int index) {
-        return const Divider(color: Colors.grey);
+        return Divider(color: Colors.grey);
       },
       itemBuilder: (BuildContext context, int index) {
-        return Text(
-          todos[index].desc,
-          style: const TextStyle(fontSize: 20.0),
+        return Dismissible(
+          key: ValueKey(todos[index].id),
+          background: showBackground(0),
+          secondaryBackground: showBackground(1),
+          onDismissed: (_) {
+            context.read<TodoListCubit>().removeTodo(todos[index]);
+          },
+          confirmDismiss: (_) {
+            return showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text('Are you sure?'),
+                  content: Text('Do you really want to delete?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, false),
+                      child: Text('NO'),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, true),
+                      child: Text('YES'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: TodoItem(todo: todos[index]),
         );
       },
+    );
+  }
+
+  Widget showBackground(int direction) {
+    return Container(
+      margin: const EdgeInsets.all(4.0),
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      color: Colors.red,
+      alignment: direction == 0 ? Alignment.centerLeft : Alignment.centerRight,
+      child: const Icon(
+        Icons.delete,
+        size: 20.0,
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class TodoItem extends StatefulWidget {
+  final Todo todo;
+
+  const TodoItem({required this.todo, Key? key}) : super(key: key);
+
+  @override
+  State<TodoItem> createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Checkbox(
+        value: widget.todo.completed,
+        onChanged: (bool? checked) {},
+      ),
+      title: Text(widget.todo.desc),
     );
   }
 }
